e88e876d6804559a988faa79db4a2de7a53d0cd3
icquirogac <icquirogac@unal.edu.co>
2019-12-17 00:27:11 -0500

    :sparkles: new features on login and register provider

--- Angular/car-app-front/src/app/components/nav-bar-provider/nav-bar-provider.component.ts
+++ Angular/car-app-front/src/app/components/nav-bar-provider/nav-bar-provider.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar-provider',
-  templateUrl: './nav-bar-provider.component.html',
-  styleUrls: ['./nav-bar-provider.component.css']
+  templateUrl: './nav-bar-provider.component.html'
 })
 export class NavBarProviderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService,
+              private router: Router ) { }
 
   ngOnInit() {
   }
 
+  logOut() {
+    this.userService.logOut();
+    this.router.navigate(['/logInProvider']);
+  }
 }
